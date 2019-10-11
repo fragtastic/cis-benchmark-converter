81cad380c6de50dd3a9f6cb8ec4032d71ac234ff
fragtastic <fragtastic@users.noreply.github.com>
2019-10-10 17:15:02 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # cis-benchmark-to-csv
-Converts text dumps from CIS Benchmark PDFs to CSV format.
+Converts dumped text from CIS Benchmark PDFs into usable CSV files.
+
+## Requirements
+- Python 3
+
+## Program Usage
+```
+usage: cisConv.py [-h] inputFile
+
+positional arguments:
+  inputFile       CIS text dump to parse.
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+
+## Output
+Below are some sample outputs of the script.
+
+```
+Parsing CIS Apple macOS 10.13 Benchmark v1.0.0.txt
+Writing to CIS Apple macOS 10.13 Benchmark v1.0.0.txt.csv
+Total lines: 2424
+Written Rows: 111
+
+Parsing CIS Microsoft Windows 10 Enterprise (Release 1809) Benchmark v1.6.0.txt
+Writing to CIS Microsoft Windows 10 Enterprise (Release 1809) Benchmark v1.6.0.txt.csv
+Total lines: 15367
+Written Rows: 507
+
+Parsing CIS Microsoft Windows Server 2019 RTM (Release 1809) Benchmark v1.0.0.txt
+Writing to CIS Microsoft Windows Server 2019 RTM (Release 1809) Benchmark v1.0.0.txt.csv
+Total lines: 12190
+Written Rows: 382
+
+Parsing CIS Microsoft Windows Server 2016 RTM (Release 1607) Benchmark v1.1.0.txt
+Writing to CIS Microsoft Windows Server 2016 RTM (Release 1607) Benchmark v1.1.0.txt.csv
+Total lines: 11704
+Written Rows: 371
+
+Parsing CIS Red Hat Enterprise Linux 7 Benchmark v2.2.0.txt
+Writing to CIS Red Hat Enterprise Linux 7 Benchmark v2.2.0.txt.csv
+Total lines: 4361
+Written Rows: 225
+
+Parsing CIS Red Hat Enterprise Linux 6 Benchmark v2.1.0.txt
+Writing to CIS Red Hat Enterprise Linux 6 Benchmark v2.1.0.txt.csv
+Total lines: 4339
+Written Rows: 225
+
+Parsing CIS Apple iOS 12 Benchmark v1.0.0.txt
+Writing to CIS Apple iOS 12 Benchmark v1.0.0.txt.csv
+Total lines: 2254
+Written Rows: 67
+
+Parsing CIS Google Android Benchmark v1.2.0.txt
+Writing to CIS Google Android Benchmark v1.2.0.txt.csv
+Total lines: 1644
+Written Rows: 43
+```
---
+++ cisConv.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import re
+import pprint
+import code
+
+# https://www.debuggex.com/
+# So far this works on every file tested.
+searcher = re.compile(r'^((?P<cisnum>(\d+\.)+\d+)\s)(\((?P<level>.+?)\)\s)?((?P<policy>.+))(\s\((?P<scored>Scored|Not\ Scored)\))$')
+
+garbage_list = [
+    '| P a g e'
+]
+
+modes = [
+    'Profile Applicability',
+    'Description',
+    'Rationale',
+    'Audit',
+    'Remediation',
+    'Impact',
+    'Default Value',
+    'References',
+    'CIS Controls'
+]
+
+def buildBlank():
+    return {
+        'Benchmark': None,
+        'CIS #': '',
+        'Scored': '',
+        'Type': '',
+        'Policy': '',
+        'Profile Applicability': [],
+        'Description': '',
+        'Rationale': '',
+        'Audit': '',
+        'Remediation': '',
+        'Impact': '',
+        'Default Value': '',
+        'References': '',
+        'CIS Controls': ''
+    }
+
+def parseText(inFileName):
+    with open(inFileName, 'rt') as inFile:
+        print(f'Parsing {inFileName}')
+        outFileName = f'{inFileName}.csv'
+        with open(outFileName, 'wt') as outFile:
+            print(f'Writing to {outFileName}')
+            cw = csv.DictWriter(outFile, fieldnames=list(buildBlank().keys()))
+            cw.writeheader()
+            metrics_total = 0
+            metrics_good = 0
+
+            row = None
+            cur_mode = None
+            force_write = False
+
+            for line in inFile:
+                metrics_total += 1
+                # line = line.strip()
+                # Skip garbage lines here
+                if any(ele in line for ele in garbage_list):
+                    continue
+
+                match = searcher.match(line)
+                if match or force_write:
+                    # Check if row is created, skips initial blanks
+                    if row:
+                        if row['Type'] == None:
+                            if len(row['Profile Applicability']) == 1:
+                                row['Type'] = row['Profile Applicability'][0]
+                            else:
+                                row['Type'] = 'See Profile Applicability'
+                        for key in row.keys():
+                            if isinstance(row[key], list):
+                                row[key] = '\n'.join(row[key])
+                        cw.writerow(row)
+                        # pprint.pprint(row)
+                        metrics_good += 1
+                        if force_write:
+                            break
+                    row = buildBlank()
+                    # code.interact(local=locals())
+                    row['Benchmark'] = inFileName[:-4]
+                    row['CIS #'] = match.group('cisnum')
+                    row['Type'] = match.group('level')
+                    row['Policy'] = match.group('policy')
+                    row['Scored'] = match.group('scored')
+                    cur_mode = None
+                    continue
+
+                else:
+                    mode_set = False
+                    for mode in modes:
+                        if line.startswith(f'{mode}:'):
+                            cur_mode = mode
+                            mode_set = True
+
+                    # Only do something on the line(s) after a mode set
+                    if not mode_set and cur_mode:
+                        # # Perform sanitization here.
+                        if cur_mode == 'Profile Applicability':
+                            line = line.replace('\uf0b7 ', '', 1)
+                        line = line.replace(' ', '', 1)
+                        # check if we have transitioned to Appendix
+                        if line.startswith('Appendix: Summary Table'):
+                            cur_mode = None
+                            force_write = True
+                            continue
+
+                        if isinstance(row[cur_mode], str):
+                            row[cur_mode] += line
+                        elif isinstance(row[cur_mode], list):
+                            row[cur_mode].append(line.strip())
+                        else:
+                            print('ERROR: Bad type. This should never happen.')
+                            exit(-1)
+
+
+    print(f'Total lines: {metrics_total}')
+    print(f'Written Rows: {metrics_good}')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputFile', type=str, help='CIS text dump to parse.')
+    args = parser.parse_args()
+    parseText(args.inputFile)
